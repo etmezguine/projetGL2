50c6544bcccb7f0cadee805c96b102ca72b9e823
Alex <etmezguine@eisti.eu>
2014-04-17 20:21:37 +0200

    test commit sur projet johan

--- qcm/src/controller/user/Admin.java
+++ qcm/src/controller/user/Admin.java
@@ -2,6 +2,8 @@ package controller.user;
 
 public class Admin extends Utilisateur {
 	
+	//test commit
+	
 	private static final long serialVersionUID = 1555922979417139347L;
 
 	public Admin(final String prenom, final String nom, final String login, final String pass) {
