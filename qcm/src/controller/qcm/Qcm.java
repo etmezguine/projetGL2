7d33baba2050d50b155788e7514540062de3fe52
Alex <etmezguine@eisti.eu>
2014-04-20 16:35:36 +0200

    ajout qcm

---
+++ qcm/src/controller/qcm/Qcm.java
@@ -0,0 +1,17 @@
+package controller.qcm;
+
+import java.util.ArrayList;
+
+public class Qcm {
+	
+	private final ArrayList<Question >listeQuestion;
+	
+	public ArrayList<Question> getListeQuestion() {
+		return listeQuestion;
+	}
+
+	public Qcm (final ArrayList<Question> listeQuestion){
+		this.listeQuestion = listeQuestion;
+	}
+
+}
